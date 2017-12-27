1b191f7abbd36d850f5f5001eb930cc942e5b930
bfeinberg <bfeinberg@netflix.com>
2017-12-26 21:10:08 -0800

    Instead of purifying, inline critical CSS

--- src/application.py
+++ src/application.py
@@ -70,7 +70,7 @@ def inject_api_keys():
 
 @application.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('new_index.html')
 
 
 @application.errorhandler(500)
@@ -89,7 +89,7 @@ def server_error(e):
 
 @application.route('/form')
 def form():
-    return render_template('index.html', disabled='disabled style=' + "'color:#888;'")
+    return render_template('new_index.html', disabled='disabled style=' + "'color:#888;'")
 
 @application.route('/stravaAuthReq')
 def authenticate_with_strava():
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,37 @@
 const merge = require('webpack-merge');
-const path = require('path');
 const common = require('./webpack.common.js');
+var path = require('path');
 var webpack = require('webpack');
-const glob = require('glob');
-const PurifyCSSPlugin = require('purifycss-webpack');
-// var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const HtmlCriticalPlugin = require("html-critical-webpack-plugin");
+
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = merge(common, {
     plugins: [
-        new PurifyCSSPlugin({ paths: glob.sync(path.join(__dirname, 'src/static/scripts/jsx/*.js')), minimize:true }),
         new webpack.optimize.UglifyJsPlugin({sourceMap:true,comments:false}),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
-        new webpack.optimize.AggressiveMergingPlugin({minSizeReduce:1.3}),
+        new webpack.optimize.AggressiveMergingPlugin({minSizeReduce:1.4}),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new HtmlCriticalPlugin({
+            base: path.resolve(__dirname, 'src/'),
+            src: 'templates/base_index.html',
+            dest: 'templates/new_index.html',
+            inline: true,
+            minify: true,
+            extract: true,
+            inlineImages: true,
+            penthouse: {
+                blockJSRequests: false,
+            }
+        }),
         new CompressionPlugin({
-            deleteOriginalAssets:true
+            /*deleteOriginalAssets:true, */minRatio:0.85, cache:true
+        }),
+        new CompressionPlugin({
+            /*deleteOriginalAssets:true, */minRatio:0.85, cache:true
         }),
         // new BundleAnalyzerPlugin()
     ]
