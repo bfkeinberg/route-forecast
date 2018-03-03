599ab6eabad135bd10039564c7e6cd69c2c1a07d
bfeinberg <bfeinberg@netflix.com>
2018-03-03 07:06:27 -0800

    Put label and file control in separate columns of same row

--- src/jsx/ui/fileInput.js
+++ src/jsx/ui/fileInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Label, Input, FormGroup, UncontrolledTooltip} from 'reactstrap';
+import {Label, Input, FormGroup, UncontrolledTooltip, Row, Col} from 'reactstrap';
 import {connect} from 'react-redux';
 import {loadGpxRoute} from "../actions/actions";
 import {decideValidationStateFor} from "../routeInfoEntry";
@@ -8,18 +8,24 @@ import {decideValidationStateFor} from "../routeInfoEntry";
 const FileInput = ({loadingSource,loadingSuccess,loadGpxRoute,timezone_api_key}) => {
     const ok=decideValidationStateFor('gpx',loadingSource,loadingSuccess);
     return (
-        <FormGroup inline size='xsm'
+        <FormGroup size='sm'
                    className='formGroup d-none d-md-block'>
             <UncontrolledTooltip placement='bottom' target='routeFile'>Upload a .gpx file describing your route</UncontrolledTooltip>
-            <Label for='routeFile' size='sm' tag='b'>Route file</Label>
-            <Input bsSize='sm' tabIndex='4' type="file" name='route'
-                   accept=".gpx" id='routeFile' {...ok}
-                   onChange={event => {
-                    // nothing to encode if the URL if we're working from a local file
-                    history.pushState(null, 'nothing', location.origin);
-                    loadGpxRoute(event,timezone_api_key);
-                }
-            }/>
+            <Row noGutters>
+                <Col sm="3">
+                    <Label for='routeFile' size='sm' tag='b'>Route file</Label>
+                </Col>
+                <Col>
+                    <Input size='2' bsSize='sm' tabIndex='4' type="file" name='route'
+                           accept=".gpx" id='routeFile' {...ok}
+                           onChange={event => {
+                            // nothing to encode if the URL if we're working from a local file
+                            history.pushState(null, 'nothing', location.origin);
+                            loadGpxRoute(event,timezone_api_key);
+                        }
+                    }/>
+                </Col>
+            </Row>
         </FormGroup>
     );
 };
