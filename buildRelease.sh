253bd037ef1344fd386c8b8b8b1038c6d07dee42
bfeinberg <bfeinberg@netflix.com>
2018-04-04 22:12:00 -0700

    Script to combine build release with pushing info to Sentry, and deploying to GAE.

---
+++ buildRelease.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# do checkins, possibly resolving issues in github or sentry in the checkin comment
+# create new pending release, then add commits to it
+VERSION=$(sentry-cli releases propose-version)
+sentry-cli releases new $VERSION
+sentry-cli releases set-commits --auto $VERSION
+# run prod build, which will also publish artifacts to Sentry and finalize the release
+./node_modules/.bin/webpack --mode production --config webpack.prod.js
+# deploy build to GAE
+gcloud app deploy --project route-forecast
+# mark Sentry release as deployed
+sentry-cli releases $VERSION new
