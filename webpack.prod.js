506550638efb7cc8d7b116833bef6de175e7a1cb
bfeinberg <bfeinberg@netflix.com>
2018-05-15 22:50:43 -0700

    Additional config for pushing to Sentry.

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,6 +34,7 @@ module.exports = env => merge(common(env), {
             ignoreFile: '.sentrycliignore',
             ignore: ['node_modules', 'webpack.config.js', 'webpack.common.js', 'webpack.dev.js'],
             configFile: 'sentry.properties',
+            stripPrefix: '/dist',
             stripCommonPrefix:true,
             rewrite:true,
             urlPrefix: '/static',
