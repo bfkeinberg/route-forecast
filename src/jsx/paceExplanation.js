aba4c69e6a478f4bb875945a01da02e877c5429d
bfeinberg <bfeinberg@netflix.com>
2020-02-12 12:52:00 -0800

    Fix display problem with help text by making it a modal

--- src/jsx/paceExplanation.js
+++ src/jsx/paceExplanation.js
@@ -1,4 +1,4 @@
-import {Button, Popover, PopoverHeader, PopoverBody} from 'reactstrap';
+import {Button, Popover, PopoverHeader, PopoverBody, Modal, ModalBody, ModalHeader} from 'reactstrap';
 import rideRatingText from './cyclerouteplanner.htm';
 import React, {Component} from 'react';
 import { Icon, Intent } from "@blueprintjs/core";
@@ -24,13 +24,21 @@ class PaceExplanation extends Component {
                 <Button id='rideRatingPopup' onClick={this.toggle} color='info' size="sm">
                     <Icon icon={IconNames.HELP} iconSize={Icon.SIZE_STANDARD} intent={Intent.NONE} />
                 </Button>
-                <Popover innerClassName='rideRatingPopup' isOpen={this.state.isOpen} placement='right'
+                <Modal isOpen={this.state.isOpen} scrollable={true} toggle={this.toggle}>
+                    <ModalBody>
+                        <div dangerouslySetInnerHTML={{__html: rideRatingText}}/>
+                    </ModalBody>
+                </Modal>
+{/*
+                <Popover innerClassName='rideRatingPopup' isOpen={this.state.isOpen} placement='auto'
+                         modifiers={{positionFixed:true}}
                          toggle={this.toggle} style={{width:450, maxWidth:500}} target='rideRatingPopup'>
                     <PopoverHeader>Route forecast tool info</PopoverHeader>
                     <PopoverBody>
                         <div dangerouslySetInnerHTML={{__html: rideRatingText}}/>
                     </PopoverBody>
                 </Popover>
+*/}
             </div>
         );
     }
