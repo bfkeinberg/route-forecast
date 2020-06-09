266a99aa0ae0aed2e531bcef3401791184020a3f
bfeinberg <bfeinberg@netflix.com>
2020-06-08 20:56:07 -0700

    dev build of server no longer requires webpack build

--- src/jsx/app/app.jsx
+++ src/jsx/app/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TopLevel from './topLevel';
-import { AppContainer } from 'react-hot-loader';
 import LocationContext from '../locationContext';
 import * as Sentry from '@sentry/browser';
 
@@ -22,19 +21,11 @@ const maps_api_key = script.getAttribute('maps_api_key');
 const timezone_api_key = script.getAttribute('timezone_api_key');
 const bitly_token = script.getAttribute('bitly_token');
 
-// Grab the state from a global variable injected into the server-generated HTML
-const preloadedState = window.__PRELOADED_STATE__;
-
-// Allow the passed state to be garbage-collected
-delete window.__PRELOADED_STLoadableComponentATE__;
-
 const render = Component => {
     ReactDOM.hydrate(
-        <AppContainer>
-            <LocationContext.Provider value={{href:location.href, search:location.search, origin:location.origin}}>
-                <Component state={preloadedState} mode={mode} action={action} maps_api_key={maps_api_key} timezone_api_key={timezone_api_key} bitly_token={bitly_token}/>
-            </LocationContext.Provider>
-        </AppContainer>,
+        <LocationContext.Provider value={{href:location.href, search:location.search, origin:location.origin}}>
+            <Component mode={mode} action={action} maps_api_key={maps_api_key} timezone_api_key={timezone_api_key} bitly_token={bitly_token}/>
+        </LocationContext.Provider>,
     document.getElementById('content'));
 };
 
