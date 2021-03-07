ff9e61354efb1048d335ad206f5afdf96824c096
Brian Feinberg <brian@feinberg.com>
2021-03-06 18:06:16 -0800

    Round up hours to nearest when requesting WeatherAPI forecast

--- src/jsx/server/weatherApi.js
+++ src/jsx/server/weatherApi.js
@@ -29,8 +29,12 @@ const callWeatherApi = function (lat, lon, currentTime, distance, zone, bearing)
 
     const weatherApiKey = process.env.WEATHER_API_KEY;
     const startTime = moment(currentTime).tz(zone);
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${lat},${lon}&days=1&hour=${startTime.format('H')}&unixdt=${startTime.unix()}`;
-    console.log(`url is ${url}`);
+    let hour = startTime.minute() > 30 ? startTime.hour() + 1 : startTime.hour();
+    if (hour > 23) {
+        hour = 0;
+    }
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${lat},${lon}&days=1&hour=${hour}&unixdt=${startTime.unix()}`;
+//    console.log(`url is ${url}`);
     const forecastResult = fetch(url).then(response => {
         const result = response.json();
         return result;
@@ -41,32 +45,32 @@ const callWeatherApi = function (lat, lon, currentTime, distance, zone, bearing)
             throw forecast.error.message;
         }
         const current = forecast.forecast.forecastday[0].hour[0];
-        console.log(`current is ${JSON.stringify(current)}`);
+//        console.log(`current is ${JSON.stringify(current)}`);
         const now = moment.unix(current.time_epoch).tz(zone)
-        console.log(`now is ${now}`);
+//        console.log(`now is ${now}`);
         const hasWind = current.wind_mph !== undefined;
         const windBearing = current.wind_degree;
         const relativeBearing = hasWind && windBearing !== undefined ? getBearingDifference(bearing, windBearing) : null;
         const rainy = current.will_it_rain != 0;
-        console.log('return ' + JSON.stringify({
-            'time':now.format('h:mmA'),
-            'distance':distance,
-            'summary':current.condition.text,
-            'tempStr':`${Math.round(current.temp_f)}F`,
-            'precip':current.chance_of_rain===undefined?'<unavailable>':`${current.chance_of_rain}%`,
-            'cloudCover':current.cloud===undefined?'<unavailable>':`${(current.cloud).toFixed(1)}%`,
-            'windSpeed':!hasWind?'<unavailable>':`${Math.round(current.wind_mph)}`,
-            'lat':lat,
-            'lon':lon,
-            'temp':`${Math.round(current.temp_f)}`,
-            'fullTime':now.format('ddd MMM D h:mmA YYYY'),
-            'relBearing':relativeBearing,
-            'rainy':rainy,
-            'windBearing':windBearing,
-            'vectorBearing':bearing,
-            'gust':current.gust_mph===undefined?'<unavailable>':`${Math.round(current.gust_mph)}`,
-            'feel':current.feelslike_f===undefined?Math.round(current.temp_f):Math.round(current.feelslike_f)
-        }));
+//        console.log('return ' + JSON.stringify({
+//            'time':now.format('h:mmA'),
+//            'distance':distance,
+//            'summary':current.condition.text,
+//            'tempStr':`${Math.round(current.temp_f)}F`,
+//            'precip':current.chance_of_rain===undefined?'<unavailable>':`${current.chance_of_rain}%`,
+//            'cloudCover':current.cloud===undefined?'<unavailable>':`${(current.cloud).toFixed(1)}%`,
+//            'windSpeed':!hasWind?'<unavailable>':`${Math.round(current.wind_mph)}`,
+//            'lat':lat,
+//            'lon':lon,
+//            'temp':`${Math.round(current.temp_f)}`,
+//            'fullTime':now.format('ddd MMM D h:mmA YYYY'),
+//            'relBearing':relativeBearing,
+//            'rainy':rainy,
+//            'windBearing':windBearing,
+//            'vectorBearing':bearing,
+//            'gust':current.gust_mph===undefined?'<unavailable>':`${Math.round(current.gust_mph)}`,
+//            'feel':current.feelslike_f===undefined?Math.round(current.temp_f):Math.round(current.feelslike_f)
+//        }));
         return {
             'time':now.format('h:mmA'),
             'distance':distance,
