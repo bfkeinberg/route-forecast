9f84edcad61d8fd89fae8940a65487ea3c715174
EthanFe <sneakcow@gmail.com>
2021-12-25 23:18:07 -0800

    remove attempt at provider wrapping

--- __test__/recalculate.test.js
+++ __test__/recalculate.test.js
@@ -7,28 +7,27 @@ import { configure, shallow } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { DateTime } from 'luxon';
 import Recalculate from '../src/jsx/ForecastSettings/Recalculate';
-import { Provider } from 'react-redux';
 
 configure({ adapter: new Adapter() });
 
 describe('<Recalculate />', () => {
     it('verify recalc is called', () => {
         const recalc = jest.fn();
-        const wrapper = shallow(<Provider>
+        const wrapper = shallow(
             <Recalculate start={DateTime.fromISO("2018-07-31T14:00:00.981Z")} pace={'D'} interval={1}
                 recalcRoute={recalc} metric={false} controls={[]} gpxRouteData={null}
                 rwgpsRoute={102766} />
-                                                </Provider>);
+        );
 
         expect(recalc.mock.calls.length).toEqual(1);
     });
 
     it('no recalc when key data is missing', () => {
         const recalc = jest.fn();
-        const wrapper = shallow(<Provider><Recalculate start={null} pace={'D'} interval={1}
+        const wrapper = shallow(<Recalculate start={null} pace={'D'} interval={1}
                                              recalcRoute={recalc} metric={false} controls={[]} gpxRouteData={null}
                                              rwgpsRoute={102766}
-        /></Provider>);
+        />);
 
         expect(recalc.mock.calls.length).toEqual(0);
     });
