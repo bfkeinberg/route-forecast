043b19966cc422d83da3ca17bc60ee86272b49a1
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2022-01-25 22:20:47 -0800

    more usable slider

--- src/jsx/ForecastSettings/ForecastInterval.js
+++ src/jsx/ForecastSettings/ForecastInterval.js
@@ -2,24 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup } from "@blueprintjs/core";
 import { Tooltip2 } from "@blueprintjs/popover2";
-import {Slider} from "@blueprintjs/core";
+//import {Slider} from "@blueprintjs/core";
+import Slider from "@mui/material/Slider"
 import {connect} from 'react-redux';
 import {setInterval} from "../../redux/actions";
 
 const ForecastInterval = ({interval,setInterval}) => {
     return (
         <FormGroup style={{flex: 1}} label='Forecast Interval'>
-            <Tooltip2 usePortal={true} placement='bottom' content='How often to generate weather forecast'>
+            <Tooltip2 usePortal={true} placement='bottom' content='How often to generate weather forecast in minutes'>
                 <Slider
                     value={interval}
-                    labelPrecision={1}
-                    labelStepSize={0.5}
-                    stepSize={0.05}
-                    min={0.5}
+                    step={0.25}
+                    min={0.25}
                     max={2.0}
-                    labelRenderer={value=>`${(value*60).toFixed()} minutes`}
-                    intent="primary"
-                    onRelease={selected => setInterval(selected)}/>
+                    marks={[{value:0.25,label:'15'},{value:0.5,label:'30'},{value:0.75,label:'45'},{value:1,label:'60'},{value:1.25,label:'75'},
+                    {value:1.5,label:'90'},{value:1.75,label:'105'},{value:2.0,label:'120'}]}
+                    valueLabelDisplay="off"
+                    onChange={(event, selected) => setInterval(selected)}/>
             </Tooltip2>
         </FormGroup>
     );
