e42445c3fcddc83ab4f5122838477bd9a7feecc1
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2022-01-28 17:27:16 -0800

    Revise test to work with altered action flow

--- __test__/requestForecast.test.js
+++ __test__/requestForecast.test.js
@@ -35,17 +35,17 @@ describe('request forecast', () => {
             })
         
         const expectedActions = [
-            { type: actions.BEGIN_FETCHING_FORECAST },
-            { type: actions.FORECAST_FETCH_SUCCESS, forecastInfo: {forecast:forecast}, finishTime: "Thu, Dec 30 2021 4:25PM", weatherCorrectionMinutes: 3.1912859550070225, maxGustSpeed: 12},
-            { type: actions.UPDATE_CALCULATED_VALUES, values:calculatedControls }
+            { type: actions.BEGIN_FETCHING_FORECAST, abortMethod:expect.any(Function) },
+            { type: actions.FORECAST_FETCH_SUCCESS, forecastInfo: {forecast:forecast}, timeZoneId: "America/Los_Angeles"}
         ];
 
         let store = mockStore(initialState);
         let v = await(store.dispatch(actions.requestForecast()));
+        let actual = store.getActions();
         expect(store.getActions()).toEqual(expectedActions);
 
         let newState = expectedActions.reduce((previousValue, currentValue) => rootReducer(previousValue,currentValue),initialState);
-        expect(newState.controls.calculatedControlValues).toEqual(calculatedControls);
+        expect(newState.forecast.forecast).toEqual(forecast);
 
         store = mockStore(newState);
         await(store.dispatch(actions.requestForecast()));
