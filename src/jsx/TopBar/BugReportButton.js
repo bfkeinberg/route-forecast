d73132f9c43c55bb7c9473beb968c234e909f7a9
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2022-03-16 14:29:25 -0700

    Use Sentry bug reporting dialog instead of directly linking to github

--- src/jsx/TopBar/BugReportButton.js
+++ src/jsx/TopBar/BugReportButton.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import LadyBug from 'Images/gustavorezende_lady_bug-555px.png';
 import {Button} from '@blueprintjs/core';
+import * as Sentry from '@sentry/browser';
+import * as SentryReact from "@sentry/react";
 
-const bugReportUrl = "https://github.com/bfkeinberg/route-forecast/issues";
+const bugReportDialog = () => {
+        const event = Sentry.captureMessage('bug report');
+        SentryReact.showReportDialog({eventId:event,
+                title:"Looks like you want to report a problem",
+                subtitle:"Please provide the details",
+                labelSubmit:"Submit bug report"
+        });
+};
+
 const BugReportButton = () => {
     return (
         <Button className="pt-intent-warning pt-small pt-minimal"
-                onClick={() => window.open(bugReportUrl,"_blank")}
+                onClick={bugReportDialog}
                 style={{height: "40px"}}
         >
                 <img style={{height: "auto"}} id='bugImage' src={LadyBug}/>Report a bug
