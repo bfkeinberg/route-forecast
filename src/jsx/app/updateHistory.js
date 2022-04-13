478fc1eaab333b52e1c6b97c93d2ee30bfa2681a
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2022-04-12 21:23:58 -0700

    split out of QueryString

---
+++ src/jsx/app/updateHistory.js
@@ -0,0 +1,18 @@
+import * as Sentry from '@sentry/react';
+
+export const updateHistory = (url, query) => {
+    Sentry.addBreadcrumb({
+        category: 'history',
+        level: Sentry.Severity.Info,
+        data: query,
+        message: url
+    });
+    if (typeof window !== 'undefined' && !(/HeadlessChrome/).test(window.navigator.userAgent) && query !== null) {
+        let oldState = history.state;
+        if (oldState && query && oldState.rwgpsRoute === query.rwgpsRoute) {
+            history.replaceState(query, 'nothing', url);
+        } else {
+            history.pushState(query, 'nothing', url);
+        }
+    }
+};
