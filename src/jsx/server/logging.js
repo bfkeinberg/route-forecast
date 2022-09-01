84da0dc1f84b78d3ee424d93275dbee277dcbed0
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2022-09-01 12:44:03 -0700

    Remove unused file

--- src/jsx/server/logging.js
+++
@@ -1,5 +0,0 @@
-// Activate Google Cloud Trace and Debug when in production
-if (process.env.NODE_ENV === 'production') {
-    require('@google-cloud/trace-agent').start({serviceContext: {enableCanary:false}});
-    require('@google-cloud/debug-agent').start({serviceContext: {enableCanary:false}});
-}
