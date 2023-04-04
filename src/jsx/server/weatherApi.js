949991aa858dc97960253f7e719d5b984c074387
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2023-04-04 16:53:44 -0700

    enable history API for WeatherAPI

--- src/jsx/server/weatherApi.js
+++ src/jsx/server/weatherApi.js
@@ -1,5 +1,5 @@
-const moment = require('moment-timezone');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { DateTime } = require("luxon");
+const axios = require('axios');
 
 //
 //
@@ -18,54 +18,48 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
  * lat: *, lon: *, temp: string, fullTime: *, relBearing: null, rainy: boolean, windBearing: number,
  * vectorBearing: *, gust: string} | never>} a promise to evaluate to get the forecast results
  */
-const callWeatherApi = function (lat, lon, currentTime, distance, zone, bearing, getBearingDifference) {
+const callWeatherApi = async function (lat, lon, currentTime, distance, zone, bearing, getBearingDifference) {
     const weatherApiKey = process.env.WEATHER_API_KEY;
-    const startTime = moment(currentTime).tz(zone);
-    let hour = startTime.minute() > 30 ? startTime.hour() + 1 : startTime.hour();
+    const startTime = DateTime.fromISO(currentTime, {zone:zone});
+    let hour = startTime.minute > 30 ? startTime.hour + 1 : startTime.hour;
     if (hour > 23) {
         hour = 0;
     }
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${lat},${lon}&days=1&hour=${hour}&unixdt=${startTime.unix()}`;
-    const forecastResult = fetch(url).then(response => {
-        const result = response.json();
-        return result;
-    }).
-    then(forecast => {
-        if (forecast.error !== undefined && forecast.error.code !== undefined) {
-            console.error(`got error code ${forecast.error.message}`);
-            throw forecast.error.message;
-        }
-        const current = forecast.forecast.forecastday[0].hour[0];
-        const now = moment.unix(current.time_epoch).tz(zone)
-        const hasWind = current.wind_mph !== undefined;
-        const windBearing = current.wind_degree;
-        const relativeBearing = hasWind && windBearing !== undefined ? getBearingDifference(bearing, windBearing) : null;
-        const rainy = current.will_it_rain !== 0;
-        return {
-            'time':now.format('h:mmA'),
-            'distance':distance,
-            'summary':current.condition.text,
-            'tempStr':`${Math.round(current.temp_f)}F`,
-            'precip':current.chance_of_rain===undefined?'<unavailable>':`${current.chance_of_rain}%`,
-            'cloudCover':current.cloud===undefined?'<unavailable>':`${current.cloud.toFixed(1)}%`,
-            'windSpeed':!hasWind?'<unavailable>':`${Math.round(current.wind_mph)}`,
-            'lat':lat,
-            'lon':lon,
-            'temp':`${Math.round(current.temp_f)}`,
-            'fullTime':now.format('ddd MMM D h:mmA YYYY'),
-            'relBearing':relativeBearing,
-            'rainy':rainy,
-            'windBearing':windBearing,
-            'vectorBearing':bearing,
-            'gust':current.gust_mph===undefined?'<unavailable>':`${Math.round(current.gust_mph)}`,
-            'feel':current.feelslike_f===undefined?Math.round(current.temp_f):Math.round(current.feelslike_f)
-        }
-    }).
-    catch(error => {
-        console.error('error',JSON.stringify(error));
-        throw error;
+    const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${lat},${lon}&days=1&hour=${hour}&aqi=yes&unixdt=${startTime.toUnixInteger()}`;
+    const historyUrl = `https://api.weatherapi.com/v1/history.json?key=${weatherApiKey}&q=${lat},${lon}&hour=${hour}&unixdt=${startTime.toUnixInteger()}`;
+    const url = startTime < DateTime.now() ? historyUrl : forecastUrl;
+    const forecastResult = await axios.get(url).catch(error => {
+        throw Error(error.response.data.error.message);
     });
-    return forecastResult;
+    if (forecastResult.data.error !== undefined && forecastResult.data.error.code !== undefined) {
+        console.error(`got error code ${forecastResult.error.message}`);
+        throw Error(forecastResult.error.message);
+    }
+    const current = forecastResult.data.forecast.forecastday[0].hour[0];
+    const now = DateTime.fromSeconds(current.time_epoch, {zone:zone});
+    const hasWind = current.wind_mph !== undefined;
+    const windBearing = current.wind_degree;
+    const relativeBearing = hasWind && windBearing !== undefined ? getBearingDifference(bearing, windBearing) : null;
+    const rainy = current.will_it_rain !== 0;
+    return {
+        'time':now.toFormat('h:mm a'),
+        'distance':distance,
+        'summary':current.condition.text,
+        'tempStr':`${Math.round(current.temp_f)}F`,
+        'precip':current.chance_of_rain===undefined?'<unavailable>':`${current.chance_of_rain}%`,
+        'cloudCover':current.cloud===undefined?'<unavailable>':`${current.cloud.toFixed(1)}%`,
+        'windSpeed':!hasWind?'<unavailable>':`${Math.round(current.wind_mph)}`,
+        'lat':lat,
+        'lon':lon,
+        'temp':`${Math.round(current.temp_f)}`,
+        'fullTime':now.toFormat('EEE MMM d h:mma yyyy'),
+        'relBearing':relativeBearing,
+        'rainy':rainy,
+        'windBearing':windBearing,
+        'vectorBearing':bearing,
+        'gust':current.gust_mph===undefined?'<unavailable>':`${Math.round(current.gust_mph)}`,
+        'feel':current.feelslike_f===undefined?Math.round(current.temp_f):Math.round(current.feelslike_f)
+    }
 };
 
 module.exports = callWeatherApi;
