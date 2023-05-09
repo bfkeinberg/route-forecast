36a38d464bb8019bfdec7229754eac29010b6a6d
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2023-05-09 11:58:34 -0700

    Enable Sentry session replay

--- src/jsx/app/app.jsx
+++ src/jsx/app/app.jsx
@@ -37,11 +37,19 @@ if (!window.origin.startsWith('http://localhost')) {
                     /^\//,
                     "www.randoplan.com"
                 ]
-            })
+            }
+            ),
+            new Sentry.Replay()
         ],
 
         // To set a uniform sample rate
-        tracesSampleRate: 0.15
+        tracesSampleRate: 0.15,
+        // This sets the sample rate to be 10%. You may want this to be 100% while
+        // in development and sample at a lower rate in production
+        replaysSessionSampleRate: 0.1,
+        // If the entire session is not sampled, use the below sample rate to sample
+        // sessions when an error occurs.
+        replaysOnErrorSampleRate: 1.0,
         /*,
         beforeBreadcrumb(breadcrumb) {
             if (breadcrumb.category==='console') {return null} else {return breadcrumb}
