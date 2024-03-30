9791d69b3b557c4f8f50b6040d66a3cfa038cfcf
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-03-30 12:01:53 -0700

    Allow clearing route id, removed redundant and ineffective methods

--- src/jsx/RouteInfoForm/RideWithGpsId.js
+++ src/jsx/RouteInfoForm/RideWithGpsId.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {InputGroup, FormGroup} from '@blueprintjs/core';
+import {InputGroup, FormGroup, Button} from '@blueprintjs/core';
 import {connect} from 'react-redux';
 import { routeDataCleared, rwgpsRouteSet } from '../../redux/reducer';
-import { getRouteNumberFromValue } from '../../utils/util';
+
 
 export const decideValidationStateFor = (type, methodUsed, loadingSuccess) => {
     if (type === methodUsed) {
@@ -18,28 +18,12 @@ export const decideValidationStateFor = (type, methodUsed, loadingSuccess) => {
 }
 
 const RideWithGpsId = ({rwgpsRouteSet,loadingSource,loadingSuccess,rwgpsRoute,routeDataCleared,loadButtonRef}) => {
-    const handleRwgpsRoute = function(value) {
-        let route = getRouteNumberFromValue(value);
-        if (route !== '') {
-            if (isNaN(route)) {
-                return;
-            }
-            // clear file input to avoid confusion
-            const routeFileField = document.getElementById('routeFile');
-            if (routeFileField != null) {
-                routeFileField.value = null;
-            }
-        }
-    };
 
     const isNumberKey = function(event) {
         const charCode = (event.which) ? event.which : event.keyCode;
         if (charCode === 13) {
             loadButtonRef.current.click()
         }
-        if ((charCode < 48 || charCode > 57))
-            return false;
-        return charCode;
     };
 
     const settingRoute = (route) => {
@@ -47,14 +31,17 @@ const RideWithGpsId = ({rwgpsRouteSet,loadingSource,loadingSuccess,rwgpsRoute,ro
         routeDataCleared();
     };
 
+    const clearRoute = () => {
+        settingRoute('')
+    }
+
     return (
         <FormGroup inline={false} style={{fontSize:"90%"}} label={<span><b>Route ID</b></span>} labelFor={'rwgps_route'} >
             <InputGroup id={'rwgps_route'} className={'glowing_input'}
-                   autoFocus tabIndex='0' type="text"
+                   autoFocus tabIndex='0' type="text" rightElement={<Button minimal icon="delete" onClick={clearRoute}></Button>}
                    {...decideValidationStateFor('rwgps',loadingSource,loadingSuccess)}
-                 onBlur={event => {handleRwgpsRoute(event.target.value)}}
                  onKeyDown={isNumberKey}
-                 onChange={event => {settingRoute(event.target.value)}}
+                 onValueChange={settingRoute}
                  onDrop={event => {
                      let dt = event.dataTransfer;
                      if (dt.items) {
@@ -63,19 +50,13 @@ const RideWithGpsId = ({rwgpsRouteSet,loadingSource,loadingSuccess,rwgpsRoute,ro
                                  event.preventDefault();
                                  dt.items[i].getAsString(value => {
                                      settingRoute(value);
-                                     handleRwgpsRoute(value);
                                  });
                              } else {
-                                 console.log('vetoing drop of',i,dt.items[i].kind);
                                  return false;
                              }
                          }
                      }
                  }}
-                 /*onDragOver={event => {
-                     event.preventDefault();
-                     event.dataTransfer.dropEffect = 'move';
-                 }}*/
                  onDragEnd={event => {
                      let dt = event.dataTransfer;
                      if (dt.items) {
@@ -102,7 +83,7 @@ RideWithGpsId.propTypes = {
     ]),
     loadButtonRef:PropTypes.oneOfType([
         PropTypes.func,
-        PropTypes.shape({ current: PropTypes.instanceOf(Component) })
+        PropTypes.shape({ current: PropTypes.instanceOf(Object) })
     ])
 };
 
