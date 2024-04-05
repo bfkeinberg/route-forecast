22f71b11dc629f9c0629faf193683281e1d32522
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-04-04 17:47:22 -0700

    turn off lint warning

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
