85297d15acf63df3ba73af12df3341223bab04e9
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-04-18 22:07:33 -0700

    attribution links will open in new tabs

--- src/jsx/resultsTables/ForecastTable.js
+++ src/jsx/resultsTables/ForecastTable.js
@@ -23,13 +23,13 @@ import { WeatherCorrections } from './WeatherCorrections';
 const displayBacklink = (provider) => {
     switch (provider) {
         case 'climacell':
-            return <a tabIndex='-1' href="https://www.tomorrow.io/"><img src={climacell} width={166} height={19} target="_blank" rel="noopener noreferrer"/></a>;
+            return <a tabIndex='-1' href="https://www.tomorrow.io/" target="_blank" rel="noopener noreferrer"><img src={climacell} width={166} height={19}/></a>;
         case 'weatherapi':
-            return <a href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0" target="_blank" rel="noopener noreferrer"/></a>;
+            return <a href="https://www.weatherapi.com/" title="Free Weather API"  target="_blank" rel="noopener noreferrer"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"/></a>;
         case 'visualcrossing':
-           return <a href="https://www.visualcrossing.com/weather-data"><img src={visualcrossing} width={200} height={100} target="_blank" rel="noopener noreferrer"/></a>;
+           return <a href="https://www.visualcrossing.com/weather-data" target="_blank" rel="noopener noreferrer"><img src={visualcrossing} width={200} height={100}/></a>;
         case 'nws':
-            return <img src={"https://www.weather.gov/images/gjt/newsletter/NWSLogo.png"} width={100} height={100}/>;
+            return <a href="https://www.weather.gov/documentation/services-web-api" target="_blank" rel="noopener noreferrer"><img src={"https://www.weather.gov/images/gjt/newsletter/NWSLogo.png"} width={100} height={100}/></a>
         case 'meteomatics':
             return meteomatics;
         case 'weatherKit':
