8bbea2d9c8f2d5dc27a048851f51b6308038fbbe
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-05-07 12:14:07 -0700

    fixes: randoplan-p3 check for the presence of route data before loading the map and also log the number of course points to Sentry for if there is an error anyway

--- src/jsx/Map/MapLoader.jsx
+++ src/jsx/Map/MapLoader.jsx
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/react";
 import lazyRetry from "@tdotcode/react-lazy-retry";
 import PropTypes from 'prop-types';
 import React, {Suspense} from 'react';
-import {connect} from 'react-redux';
+import {connect, useSelector} from 'react-redux';
 
 import { routeLoadingModes } from '../../data/enums';
 import ErrorBoundary from "../shared/ErrorBoundary";
@@ -18,11 +18,17 @@ const addBreadcrumb = (msg) => {
 const LoadableMap = lazyRetry(() => {addBreadcrumb('loading map');return import(/* webpackChunkName: "Map" */ './RouteForecastMap')}, 7, 1200);
 
 const MapLoader = (props) => {
-    if (props.hasMap) {
+    // debug empty points and bounds
+    const rwgpsRouteData = useSelector(state => state.routeInfo.rwgpsRouteData)
+    let length = 0
+    if (rwgpsRouteData && rwgpsRouteData[rwgpsRouteData.type] && rwgpsRouteData[rwgpsRouteData.type]['track_points']) {
+        length = rwgpsRouteData[rwgpsRouteData.type]['track_points'].length
+    }
+    if (props.hasMap && props.hasRouteData) {
         Sentry.addBreadcrumb({
             category: 'load',
             level: 'info',
-            message:'Loading map'
+            message:`Loading map with ${length} route points`
         })
         return <ErrorBoundary>
             <Suspense fallback={<div>Loading Map...</div>}>
@@ -34,15 +40,12 @@ const MapLoader = (props) => {
     }
 };
 
-MapLoader.propTypes = {
-    hasMap:PropTypes.bool.isRequired
-};
-
 const mapStateToProps = (state) =>
     ({
         // TODO
         // this condition is not quite right -- it doesn't catch cases where we're in strava mode but no strava activity data has loaded
         // should change this and probably move it to a higher level component, when replacing the existing placeholder text with a splash screen or w/e
+        hasRouteData: (state.routeInfo.rwgpsRouteData || state.routeInfo.gpxRouteData || state.strava.activityStream),
         hasMap: (state.forecast.forecast.length > 0 || state.uiInfo.routeParams.routeLoadingMode === routeLoadingModes.STRAVA)
     });
 
