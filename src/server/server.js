569193123252b6d3f74ec4b2cd209faba63ec9c4
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-05-26 17:54:42 -0700

    fix Sentry integration at server startup

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,5 @@
 /* eslint-disable max-lines */
+require('./instrument');
 const express = require('express');
 const app = express();
 require('source-map-support').install();
@@ -22,17 +23,6 @@ const getAirNowAQI = require('./airNowAQI');
 const querystring = require('querystring');
 const Sentry = require('@sentry/node');
 
-if (process.env.NODE_ENV !== 'development') {
-    Sentry.init({
-        dsn: 'https://ea4c472ff9054dab8c18d594b95d8da2@sentry.io/298059',
-        integrations: [
-            // enable HTTP calls tracing
-            new Sentry.Integrations.Http({ tracing: true }),
-            new Sentry.Integrations.Express({ app })
-        ],
-        tracesSampleRate: 0.15
-    });
-}
 let logger = console;
 
 var compression = require('compression');
