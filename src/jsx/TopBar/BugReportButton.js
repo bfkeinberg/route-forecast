a61f5587aade3281051aef674e84d1cf2b62384a
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-06-03 22:15:17 -0700

    fixes: randoplan-qz The feedback dialog like everything else in Sentry has changed in current SDK and needed code updates to use.

--- src/jsx/TopBar/BugReportButton.js
+++ src/jsx/TopBar/BugReportButton.js
@@ -1,25 +1,27 @@
 import {Button} from '@blueprintjs/core';
 import { getClient } from "@sentry/react"
 import LadyBug from 'Images/gustavorezende_lady_bug-555px.png';
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import {useTranslation} from 'react-i18next'
 
-const openSentryDialog = () => {
-        const client = getClient();
-        if (client) {
-                const feedback = client.getIntegrationByName("Feedback")
-                if (feedback) feedback.openDialog()
-        }
-}
-
 const BugReportButton = () => {
+        const buttonRef = useRef()
         const { t } = useTranslation()
+        useEffect(() => {
+                const client = getClient();
+                if (client) {
+                        const feedback = client.getIntegrationByName("Feedback")
+                        if (buttonRef.current) {
+                                feedback.attachTo(buttonRef.current)
+                        }
+                }        
+        })
         return (
                 <Button className="pt-intent-warning"
-                        onClick={openSentryDialog}
                         style={{ height: "47px" }}
                         text={t('buttons.bugReport')}
                         small
+                        ref={buttonRef}
                 >
                         <img style={{ height: "45", width: "32" }} id='bugImage' src={LadyBug} />
                 </Button>
