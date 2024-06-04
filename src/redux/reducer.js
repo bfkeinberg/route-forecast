8b6c7bc9a682b89f30d7c05cdd6d88cf9b7f5a4d
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-06-04 11:58:37 -0700

    Set WeatherKit to 15 minute intervals to match up with the slider

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -25,7 +25,7 @@ export const providerValues = {
     visualcrossing:{min_interval:0.25,max_days:14, canForecastPast:true, daysInPast:4, name:"Visual Crossing", enabled:true},
     nws:{min_interval:1,max_days:7, canForecastPast:false, daysInPast:0, name:"National Weather Service", enabled:true},
     // meteomatics:{min_interval:1,max_days:10,canForecastPast:true, daysInPast:1, name:"Meteomatics"}
-    weatherKit:{min_interval:0.2,max_days:8, canForecastPast:true, name:"Apple WeatherKit", enabled:true},
+    weatherKit:{min_interval:0.25,max_days:8, canForecastPast:true, name:"Apple WeatherKit", enabled:true},
     }
 export const defaultProvider = 'nws'
 
