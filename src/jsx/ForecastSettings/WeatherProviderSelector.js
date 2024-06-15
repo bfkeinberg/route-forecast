6fd16f36b4440d5080722298e897f678709e9302
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-06-14 20:53:31 -0700

    Reduce calling of route analysis by memoizing forecast request length

--- src/jsx/ForecastSettings/WeatherProviderSelector.js
+++ src/jsx/ForecastSettings/WeatherProviderSelector.js
@@ -1,14 +1,14 @@
 import {Button, FormGroup, MenuItem} from "@blueprintjs/core";
 import { Select } from "@blueprintjs/select";
 import PropTypes from 'prop-types';
-import React from 'react';
-import {connect} from 'react-redux';
+import React, {useMemo} from 'react';
+import {connect, useSelector} from 'react-redux';
 
 import {setWeatherProvider} from "../../redux/actions";
 import {providerValues} from "../../redux/reducer";
-import { getForecastRequestLength } from '../../utils/forecastUtilities';
 import { DesktopTooltip } from '../shared/DesktopTooltip';
 import {useTranslation} from 'react-i18next'
+import { getForecastRequest } from "../../utils/util";
 
 const renderProvider = (provider, { handleClick, handleFocus, modifiers }) => {
     if (!modifiers.matchesPredicate) {
@@ -26,8 +26,28 @@ const renderProvider = (provider, { handleClick, handleFocus, modifiers }) => {
     );
 };
 
-const WeatherProviderSelector = ({weatherProvider,setWeatherProvider,forecastLength}) => {
+const WeatherProviderSelector = ({weatherProvider,setWeatherProvider}) => {
     const { t } = useTranslation()
+    // TODO: move into separate component, design pattern as yet unclear
+    const type = useSelector(state => state.routeInfo.rwgpsRouteData) ? "rwgps" : "gpx"
+    const timeZoneId = useSelector(state => state.uiInfo.routeParams.zone)
+    const routeData = useSelector(state => state.routeInfo[type === "rwgps" ? "rwgpsRouteData" : "gpxRouteData"])
+    const startTimestamp = useSelector(state => state.uiInfo.routeParams.startTimestamp)
+    const pace = useSelector(state => state.uiInfo.routeParams.pace)
+    const interval = useSelector(state => state.uiInfo.routeParams.interval)
+    const controlPoints = useSelector(state => state.controls.userControlPoints)
+    const segment = useSelector(state => state.uiInfo.routeParams.segment)
+    const getForecastRequestLength = () => {
+        const forecastRequest = getForecastRequest(
+            routeData, 
+            startTimestamp, 
+            type, timeZoneId, pace, 
+            interval, controlPoints,
+            segment)
+        return forecastRequest.length
+    }
+    const forecastLength = useMemo(getForecastRequestLength, [routeData,interval])
+
     return (
         <FormGroup label={<span><b>{t('labels.source')}</b></span>} labelFor={'provider'}>
             <DesktopTooltip content={t('tooltips.provider')} placement={"right"}>
@@ -51,13 +71,11 @@ const WeatherProviderSelector = ({weatherProvider,setWeatherProvider,forecastLen
 WeatherProviderSelector.propTypes = {
     weatherProvider:PropTypes.string.isRequired,
     setWeatherProvider:PropTypes.func.isRequired,
-    forecastLength:PropTypes.number.isRequired
 };
 
 const mapStateToProps = (state) =>
     ({
-        weatherProvider: state.forecast.weatherProvider,
-        forecastLength: getForecastRequestLength(state)
+        weatherProvider: state.forecast.weatherProvider
     });
 
 const mapDispatchToProps = {
