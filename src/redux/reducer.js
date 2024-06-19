1e244fbdbfc49f4f1ad7d26facf8319935e3e95f
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-06-19 07:39:37 -0700

    fixes: randoplan-s6 when loading a trip rather than a route reference the proper field

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -221,7 +221,7 @@ const routeInfoSlice = createSlice({
                 state.canDoUserSegment = action.payload.route.track_points[0].d !== undefined
             } else {
                 state.distanceInKm = action.payload.trip.distance/1000
-                state.canDoUserSegment = action.payload.route.track_points[0].d !== undefined
+                state.canDoUserSegment = action.payload.trip.track_points[0].d !== undefined
             }
             state.type = "rwgps"
         },
