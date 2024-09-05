767e4efb7ffbde3926215a7f5499be2a43921f42
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-09-05 09:26:12 -0700

    reorder forecast sources somewhat

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -21,11 +21,11 @@ const initialStartTime = function() {
 
 export const providerValues = {
     nws:{min_interval:1, max_days:7, canForecastPast:false, daysInPast:0, name:"National Weather Service", enabled:true},
-    weatherKit:{min_interval:0.25, max_days:8, canForecastPast:true, name:"Apple WeatherKit", enabled:true},
+    oneCall:{min_interval:0.25, max_days:5, canForecastPast:true, daysInPast:14, name:"OneCall", enabled:true},
     visualcrossing:{min_interval:0.25, max_days:14, canForecastPast:true, daysInPast:4, name:"Visual Crossing", enabled:true},
     weatherapi:{min_interval:1, max_days:10, canForecastPast:false, daysInPast:4, name:"WeatherAPI", enabled:true},
+    weatherKit:{min_interval:0.25, max_days:8, canForecastPast:true, name:"Apple WeatherKit", enabled:true},
     climacell:{min_interval:0.25, max_days:4, canForecastPast:false, daysInPast:1, name:"Tomorrow.io", maxCallsPerHour:25, enabled:true},
-    oneCall:{min_interval:0.25, max_days:5, canForecastPast:true, daysInPast:14, name:"OneCall", enabled:true},
     // meteomatics:{min_interval:1, max_days:10,canForecastPast:true, daysInPast:1, name:"Meteomatics"}
     }
 export const defaultProvider = 'nws'
