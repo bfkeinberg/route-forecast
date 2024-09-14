a14a69540d70b0f79231d9fececfe471bd7a8bb6
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-09-14 15:10:27 -0700

    fixes: randoplan-tj Move the back-button-caused navigation back to the home tab in the mobile ui from the render method to onEffect

--- src/jsx/MobileUI.jsx
+++ src/jsx/MobileUI.jsx
@@ -48,13 +48,16 @@ const MobileUITabs = (props) => {
         const routeData = useSelector(state => state.routeInfo[type === "rwgps" ? "rwgpsRouteData" : "gpxRouteData"])
         const stravaActivityData = useSelector(state => state.strava.activityData)
         const forecastData = useSelector(state => state.forecast.forecast)
-        // if we've been reset while displaying another tab
-        if (pathname !== '/' && routeData === null) {
-            navigate('/')
-        }
         useWhenChanged(routeData, () => navigate("/controlPoints", { replace: true }), routeData !== null)
         useWhenChanged(forecastData, () => navigate("/forecastTable", { replace: true }), forecastData.length > 0)
         useWhenChanged(stravaActivityData, () => navigate("/paceTable", { replace: true }))
+
+        React.useEffect(() => {
+            // if we've been reset while displaying another tab
+            if (pathname !== '/' && routeData === null) {
+                navigate('/')
+            }
+        }), [pathname, routeData]
 
         React.useEffect(() => {
             if (props.orientationChanged) {
