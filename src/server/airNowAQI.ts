b7c1a66bb42cd6d08a464667b48e5edaad361f55
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-10-27 08:04:15 -0700

    Add type for IQAir results

--- src/server/airNowAQI.ts
+++ src/server/airNowAQI.ts
@@ -7,13 +7,18 @@ const forecastDay = DateTime.now().toFormat("y-MM-dd");
 
 const getAirNowAQI = async function (lat : number, lon : number) {
     const url = `https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=${lat}&longitude=${lon}&date=${forecastDay}&API_KEY=${airNowKey}`;
-    let airNowResult = await axios.get(url).catch((error: any) => {
+    let airNowResult = await axios.get<Array<IqAirData>>(url).catch((error: any) => {
         Sentry.captureException(error)
     });
     if (!airNowResult || airNowResult.data.length===0) {
         return undefined;
     }
-    let filteredResults = airNowResult.data.filter((obj: { ParameterName: string; AQI: number; }) => obj.ParameterName==="PM2.5" && obj.AQI!==-1);
+    interface IqAirData {
+        ParameterName: string;
+        AQI: number;
+    }
+
+    let filteredResults = airNowResult.data.filter((obj: IqAirData) => obj.ParameterName==="PM2.5" && obj.AQI!==-1);
     if (filteredResults.length===0) {
         return undefined;
     }
