10c981db3d8a82ae38ac5e24f61fb935d4fe9705
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2025-03-23 21:18:55 -0700

    turn off debug now

--- src/jsx/app/i18n.ts
+++ src/jsx/app/i18n.ts
@@ -17,7 +17,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: false,
     fallbackLng: 'en-US',
     nonExplicitSupportedLngs: true,
     supportedLngs: ['en-US', 'en-GB', 'en', 'fr', 'es', 'bg', 'hu', 'it'],
