fa37058b50ab678bc86628e17c35e94249db58d7
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2025-04-20 22:31:51 -0700

    update to get all of multiple part numbers for a given device

--- makeGarminObj.sh
+++ makeGarminObj.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-find ~/Library/Application\ Support/Garmin/ConnectIQ/Devices -name compiler.json -exec cat {} \; | jq -s | jq '.[] |= {(.worldWidePartNumber):.displayName} | add'
+find ~/Library/Application\ Support/Garmin/ConnectIQ/Devices -name compiler.json -exec cat {} \; | jq -s '.[] | {(.partNumbers[] .number):.displayName}' | jq -s add
