40d061051db06dfc9a9f9d5b0588d77b6070624f
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2025-05-14 20:00:08 -0700

    update wind effects chart

--- src/jsx/resultsTables/TimeChangeChart.tsx
+++ src/jsx/resultsTables/TimeChangeChart.tsx
@@ -1,35 +1,43 @@
-import { Card, Elevation } from '@blueprintjs/core';
 // @ts-ignore 
 import { largestTriangleThreeBucket } from 'd3fc-sample';
 import { ResponsiveContainer, LineChart, XAxis, YAxis, Tooltip, Legend, Line, CartesianGrid } from 'recharts';
 import type { ChartDataType, ChartData } from 'utils/gpxParser';
 
-export const TimeChangeChart = ({ chartData, metric }: { chartData: ChartDataType; metric: boolean; }) => {
-  const desiredDataSize = 250;
-  const sampler = largestTriangleThreeBucket();
-  sampler.x((d: ChartData) => d.distanceInKm)
-    .y((d: ChartData) => d.totalMinutesLost);
-  sampler.bucketSize(Math.max(chartData.length / desiredDataSize, 5));
+export const TimeChangeChart = (chartData: ChartDataType, metric: boolean) => {
+    const desiredDataSize = 250;
+    const sampler = largestTriangleThreeBucket();
+    sampler.x((d: ChartData) => d.distanceInKm)
+        .y((d: ChartData) => d.totalMinutesLost);
+    sampler.bucketSize(Math.max(chartData.length / desiredDataSize, 5));
 
-  const sampledData = sampler(chartData);
-  const kmToMiles = 0.62137;
+    const sampledData = sampler(chartData);
+    const kmToMiles = 0.62137;
 
-  const formatDistance = (value: number, isMetric: boolean) => isMetric ? value.toFixed(0) : (value * kmToMiles).toFixed(0);
+    const formatDistance = (value: number, isMetric: boolean) => isMetric ? value.toFixed(0): (value * kmToMiles).toFixed(0);
+    const formatTipDistance = (value: number, isMetric: boolean) => isMetric ? value.toFixed(0) + " km": (value * kmToMiles).toFixed(0) + " miles";
+    const formatWindSpeed = (speed: number, isMetric: boolean) => isMetric ? (speed/kmToMiles).toFixed(0) : speed.toFixed(0)
+    const formatTooltipValue = (value: number, name: string, isMetric: boolean) => 
+        (name === 'totalMinutesLost') ? value.toFixed(0) : isMetric ? [(value / kmToMiles).toFixed(0), "windSpeedKph"] : [value.toFixed(0), "windSpeedMph"]
 
-  return (
-    <Card interactive={true} elevation={Elevation.THREE}>
-      <ResponsiveContainer width="100%" height={"100%"} minWidth={525} minHeight={250} maxHeight={400}>
+    return <ResponsiveContainer width="100%" height={"100%"} minWidth={550} minHeight={250} /* maxHeight={400} */>
         <LineChart
-          width={525} height={250} data={sampledData}
+            width={550} height={250} data={sampledData}
+            margin={{
+                top: 30,
+                right: 35,
+                left: 30,
+                bottom: 30,
+            }}
         >
-          <XAxis dataKey="distanceInKm" type={'number'} unit={metric ? " km" : " miles"} tickFormatter={(value: number) => formatDistance(value, metric)} domain={[0, 'dataMax']} />
-          <YAxis dataKey="totalMinutesLost" unit=" mins" />
-          <Tooltip labelFormatter={(value: number) => formatDistance(value, metric)} formatter={(value: number) => value.toFixed(0)} />
-          <Legend />
-          <Line type="monotone" dataKey="totalMinutesLost" dot={false} />
-          <CartesianGrid strokeDasharray="2 2" />
+            <XAxis dataKey="distanceInKm" type={'number'} unit={metric ? " km" : " miles"} tickFormatter={(value: number) => formatDistance(value, metric)} domain={[0, 'dataMax']} />
+            <YAxis dataKey="totalMinutesLost" unit=" mins" />
+            <YAxis dataKey="windSpeedMph" type="number" unit={metric?" kph":" mph"} yAxisId="right" orientation="right" 
+                domain={['dataMin', 'dataMax']} tickFormatter={(value:number) => `${value > 0 ? '+':''}${formatWindSpeed(value, metric)}`}/>
+            <Tooltip labelFormatter={(value: number) => formatTipDistance(value, metric)} formatter={(value: number, name: string) => formatTooltipValue(value, name, metric)} />
+            <Legend />
+            <Line type="monotone" dataKey="totalMinutesLost" dot={false} />
+            <Line type="monotone" yAxisId={'right'} dataKey="windSpeedMph" stroke={"#32a852"} dot={false} name={metric?"windSpeedKph":"windSpeedMph"}/>
+            <CartesianGrid strokeDasharray="2 2" />
         </LineChart>
-      </ResponsiveContainer>
-    </Card>
-  );
-};
+    </ResponsiveContainer>
+}
--- src/jsx/resultsTables/WeatherCorrections.tsx
+++ src/jsx/resultsTables/WeatherCorrections.tsx
@@ -1,28 +1,27 @@
-import {useTranslation} from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { useForecastDependentValues, useAppSelector } from '../../utils/hooks';
-import {useState} from "react"
 const gustySpeedThreshold = 25;
+import { Popover } from '@blueprintjs/core';
 import { TimeChangeChart } from './TimeChangeChart';
 
 export const WeatherCorrections = () => {
-  const [hovering, setHovering] = useState(false)
   const isMetric = useAppSelector(state => state.controls.metric)
-  const onMouseEnter = () => setHovering(true)
-  const onMouseLeave = () => setHovering(false)
-  
+
   const { weatherCorrectionMinutes, maxGustSpeed, chartData } = useForecastDependentValues()
   const { t } = useTranslation()
 
   const weatherId = (maxGustSpeed > gustySpeedThreshold) ? 'gustyWeather' : 'weatherCorrections';
 
   const lost = weatherCorrectionMinutes >= 0
+
   return (
-    <span id={weatherId} style={{ fontSize: "14px" }}>
-      <span onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-        {hovering && <TimeChangeChart chartData={chartData} metric={isMetric}/>}
-        {Math.abs(Math.round(weatherCorrectionMinutes))} minutes <span style={{ color: lost ? "darkorange" : "deepskyblue" }}>
-          {lost ? `${t('data.wind.lost')} ` : `${t('data.wind.gained')} `}</span>{lost ? `${t('data.wind.to')}` : `${t('data.wind.from')}`}
+    <Popover interactionKind='hover' content={TimeChangeChart(chartData, isMetric)}>
+      <span id={weatherId} style={{ fontSize: "14px" }}>
+        <span>
+          {Math.abs(Math.round(weatherCorrectionMinutes))} minutes <span style={{ color: lost ? "darkorange" : "deepskyblue" }}>
+            {lost ? `${t('data.wind.lost')} ` : `${t('data.wind.gained')} `}</span>{lost ? `${t('data.wind.to')}` : `${t('data.wind.from')}`}
+        </span>
       </span>
-    </span>
+    </Popover>
   )
 }
