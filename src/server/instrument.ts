e91e660c6c90bceded14735c68db3025a5573b2d
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2025-05-16 19:39:30 -0700

    Enabling Sentry logs

--- src/server/instrument.ts
+++ src/server/instrument.ts
@@ -4,6 +4,7 @@ const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 if (process.env.NODE_ENV !== 'development') {
     Sentry.init({
         dsn: 'https://ea4c472ff9054dab8c18d594b95d8da2@sentry.io/298059',
+        _experiments: { enableLogs: true },
         integrations: [
             nodeProfilingIntegration()
         ],
