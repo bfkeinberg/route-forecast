7cf528ec6e71912be7c4695e5f3b394950018068
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2025-12-26 22:10:59 -0800

    fixes: randoplan-z3 must separate generation of short url (async) and the click to copy it

--- src/jsx/TopBar/ShortUrl.tsx
+++ src/jsx/TopBar/ShortUrl.tsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import * as Sentry from "@sentry/react";
 import type { RootState } from "../../redux/store";
 import {IconArrowUpSquare, IconCopy} from "@tabler/icons-react"
-import { TextInput, Notification, ActionIcon } from '@mantine/core';
+import { TextInput, Notification, ActionIcon, Flex } from '@mantine/core';
 import {useTranslation} from 'react-i18next'
 import { notifications } from '@mantine/notifications';
 import { useAppSelector, useAppDispatch } from "../../utils/hooks";
@@ -12,7 +12,7 @@ import { shortenUrl } from "../../redux/actions";
 import React from "react";
 const { trace, debug, info, warn, error, fatal, fmt } = Sentry.logger;
 
-const ShortUrl = ({shortUrl} : {shortUrl:string}) => {
+const ShortUrl = ({ shortUrl }: { shortUrl: string }) => {
     const { t } = useTranslation()
     const errorDetails = useAppSelector(state => state.uiInfo.dialogParams.errorDetails)
     const searchString = useAppSelector(state => state.params.searchString);
@@ -20,15 +20,16 @@ const ShortUrl = ({shortUrl} : {shortUrl:string}) => {
     const dispatch = useAppDispatch()
     const [needToGetShortUrl, setNeedToGetShortUrl] = React.useState(false);
     const [copyFailed, setCopyFailed] = React.useState(false);
+    const [showCopyButton, setShowCopyButton] = React.useState(false);
     const publicUrl = useAppSelector(state => state.params.queryString);
-    
+
     React.useEffect(() => {
         if (needToGetShortUrl && publicUrl) {
             dispatch(shortenUrl(publicUrl));
             setNeedToGetShortUrl(false);
         }
     }, [needToGetShortUrl, publicUrl]);
-    
+
     const fallbackCopyToClipboard = (text: string) => {
         const textArea = document.createElement('textarea');
         textArea.value = text;
@@ -56,26 +57,25 @@ const ShortUrl = ({shortUrl} : {shortUrl:string}) => {
         document.body.removeChild(textArea); // Clean up the temporary element
     }
 
-    const copyShortUrl = async (shortUrl : string) => {
+    const copyShortUrl = async (shortUrl: string) => {
         if (navigator.clipboard) {
             try {
                 await navigator.clipboard.writeText((shortUrl));
-                notifications.show({ message: "Short URL copied", autoClose:3000, withCloseButton: false });
+                notifications.show({ message: "Short URL copied", autoClose: 3000, withCloseButton: false });
             } catch (err) {
                 Sentry.captureException(err);
                 fallbackCopyToClipboard(shortUrl);
+                notifications.show({ message: "Short URL was copied", autoClose: 3000, withCloseButton: false });
             }
         }
         else {
             fallbackCopyToClipboard(shortUrl);
+            notifications.show({ message: "Short URL was copied", autoClose: 3000, withCloseButton: false });
         }
     }    
 
     React.useEffect(() => {
-        if (shortUrl !== 'click here to get a short URL') {
-            copyShortUrl(shortUrl);
-            ReactGA.event('share', {item_id:shortUrl})
-        }
+        setShowCopyButton(shortUrl !== 'click here to get a short URL');
     }, [shortUrl]);
     
     const showErrorNotUrl = errorDetails && searchString && distance;
@@ -87,13 +87,16 @@ const ShortUrl = ({shortUrl} : {shortUrl:string}) => {
                 <div>{t('labels.share')}</div>
             </div>
             {showErrorNotUrl && <Notification color='red' onClose={() => dispatch(errorDetailsSet(null))}>{errorDetails}</Notification>}
-            {copyFailed && <ActionIcon onClick={() => {setCopyFailed(false); copyShortUrl(shortUrl)}}>
+            <Flex>
+            {showCopyButton && <ActionIcon onClick={() => {setCopyFailed(false); copyShortUrl(shortUrl); ReactGA.event('share', {item_id: shortUrl})}}>
                 <IconCopy size={12} />
             </ActionIcon>}
             <TextInput style={textStyle} leftSection={<IconArrowUpSquare />} id={'shortUrl'} size="xs" readOnly type="text" value={shortUrl}
                 onClick={async event => {
                     setNeedToGetShortUrl(true);
-                }} />
+                }} 
+                />
+                </Flex>
         </div>
     );
 };
