7bc42351175a89b17ff5d245497bfaedebb42c72
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2026-01-15 07:40:37 -0800

    Simplifiied config files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
+const { debug } = require("node:console");
+
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', {debug: true}],
     [
       '@babel/preset-react',
       {
@@ -12,38 +14,37 @@ module.exports = {
   plugins: [
     "@babel/plugin-transform-runtime",
     "react-html-attrs",
-    "@babel/plugin-transform-class-properties",
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-syntax-import-meta",
-    "@babel/plugin-transform-json-strings",
-    [
-      "@babel/plugin-proposal-decorators",
-      {
-        "legacy": true
-      }
-    ],
-    "@babel/plugin-proposal-function-sent",
-    "@babel/plugin-transform-export-namespace-from",
-    "@babel/plugin-transform-numeric-separator",
-    "@babel/plugin-proposal-throw-expressions",
-    "@babel/plugin-proposal-export-default-from",
-    "@babel/plugin-transform-logical-assignment-operators",
-    "@babel/plugin-transform-optional-chaining",
-    [
-      "@babel/plugin-proposal-pipeline-operator",
-      {
-        "proposal": "minimal"
-      }
-    ],
-    "@babel/plugin-transform-nullish-coalescing-operator",
-    "@babel/plugin-proposal-do-expressions",
-    "@babel/plugin-proposal-function-bind"
+    // "@babel/plugin-transform-class-properties",
+    // "@babel/plugin-syntax-dynamic-import",
+    // "@babel/plugin-syntax-import-meta",
+//    "@babel/plugin-transform-json-strings",
+  //  [
+    //  "@babel/plugin-proposal-decorators",
+      //{
+        //"legacy": true
+      //}
+    //],
+    // "@babel/plugin-proposal-function-sent",
+    // "@babel/plugin-transform-export-namespace-from",
+    // "@babel/plugin-transform-numeric-separator",
+    // "@babel/plugin-proposal-throw-expressions",
+    // "@babel/plugin-proposal-export-default-from",
+    // "@babel/plugin-transform-logical-assignment-operators",
+    // "@babel/plugin-transform-optional-chaining",
+    // [
+    //   "@babel/plugin-proposal-pipeline-operator",
+    //   {
+    //     "proposal": "minimal"
+    //   }
+    // ],
+    // "@babel/plugin-transform-nullish-coalescing-operator",
+    // "@babel/plugin-proposal-do-expressions",
+    // "@babel/plugin-proposal-function-bind"
   ],
   env: {
     test: {
       "plugins": [
-        "dynamic-import-node",
-        '@babel/plugin-syntax-dynamic-import'
+        "dynamic-import-node"
       ]
     }
   }
